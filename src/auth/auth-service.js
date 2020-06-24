b5ff21409cf9119c2e6d9213fc150d28b33a6ef0
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-24 10:30:41 -0400

    added auth service and router

---
+++ src/auth/auth-service.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+const AuthService = {
+    getUserWithUsername(db, username) {
+        return db('users')
+            .where({ username })
+            .first()
+    },
+    hasUserWithUsername(db, username) {
+        return db('users')
+            .where({ username })
+            .first()
+            .then((user) => !!user)
+    },
+    insertUser(db, newUser) {
+        return db
+            .insert(newUser)
+            .into('users')
+            .returning("*")
+            .then(([user]) => user);
+    },
+    comparePasswords(password, hash) {
+        return bcrypt.compare(password, hash)
+    },
+    createJwt(subject, payload) {
+        return jwt.sign(payload, config.JWT_SECRET, {
+            subject,
+            expiresIn: config.JWT_EXPIRY,
+            algorithm: 'HS256',
+        })
+    },
+    verifyJwt(token) {
+        return jwt.verify(token, config.JWT_SECRET, {
+            algorithms: ['HS256'],
+        })
+    },
+    parseBasicToken(token) {
+        return Buffer
+            .from(token, 'base64')
+            .toString()
+            .split(':')
+    },
+    serializeUser(user) {
+        return {
+            id: user.id,
+            email: xss(user.email),
+            username: xss(user.username),
+        }
+    }
+}
+
+module.exports = AuthService
