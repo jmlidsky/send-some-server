13e6e1f70eb8593d92689cc64ed022d5e0966209
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-24 10:32:07 -0400

    added routes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
+const locationsRouter = require('./locations/locations-router')
+const authRouter = require('./auth/auth-router')
+
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -14,6 +17,9 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/api/locations', locationsRouter)
+app.use('/api/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
