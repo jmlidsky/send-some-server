37f1972a579eb308e62830346c9a9cebb85db64a
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-26 14:52:11 -0400

    commented out error message for troubleshooting

--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        // response = { error: { message: 'server error' } }
-        response = { message: error.message, error }
+        response = { error: { message: 'server error' } }
+        // response = { message: error.message, error }
     } else {
         console.error(error)
         response = { message: error.message, error }
