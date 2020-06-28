98750c8b215fcf70e84bad4a6d5553f5aaf6036a
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-27 20:14:29 -0400

    troubleshooting for client login

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -42,6 +42,8 @@ authRouter
         const { username, password } = req.body
         const loginUser = { username, password }
 
+        console.log(loginUser)
+
         for (const [key, value] of Object.entries(loginUser))
             if (value == null)
                 return res.status(400).json({
